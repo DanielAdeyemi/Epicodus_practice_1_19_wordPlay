04331d82e260a25107bb4986656481206f2d7c83
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-19 16:17:27 -0800

    add function to take words from sentence with more than 2 letters

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("form").submit(function() {
+    event.preventDefault();
+    let inputs = $(".input").val().split(" ");
+    let nAr = [];
+    inputs.forEach(function(input) {
+      if (input.length > 2) {
+        nAr.push(input);
+      }
+    });
+    alert(nAr);
+  });
+});
